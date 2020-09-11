920f2135332cf39a1a5849b44de10f79923c366a
jreyes8631 <jreyes8631@gmail.com>
2020-09-11 07:55:13 -0400

    add seeds for testing

---
+++ db/migrate/seeds.rb
@@ -0,0 +1,14 @@
+#Create 2 users:
+
+jose = User.create(name: "jose", email: "jreyes8631@gmail.com", password: "mospozey")
+naya = User.create(name: "Nayara", email: "nayare26@gmail.com", password: "mospozey2")
+
+#create 2 movie
+
+movie_one = Movie.create(title: "matrix", user_id: jose.id)
+movie_two = Movie.create(title: "beuty and the beast", user_id: naya.id, description: "Scify movie")
+
+#create 2 mreview
+
+jose_review = Review.create(movie_id: movie_one.id, user_id: jose.id, rating: "5 starts", comment: "best movie ever")
+naya_review = Review.create(movie_id: movie_two.id, user_id: naya.id, rating: "5 starts", comment: "best Disney movie ever")
