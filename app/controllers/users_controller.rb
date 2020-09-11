4dccdcaebe5ac5defbab07311e53ea6147fc14e9
jreyes8631 <jreyes8631@gmail.com>
2020-09-11 18:52:19 -0400

    add user foler to view, move signup and added route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
-class UsersController < ApplicationController 
+class UsersController < ApplicationController
+
     get "/login" do
       erb :login
     end
 
-    get "/signup" do 
-       erb :signup
+    get '/signup' do
+      erb :'users/signup'
     end
   
 end
