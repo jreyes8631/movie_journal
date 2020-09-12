bed18fa7c1901bdebe820113bb4951526a709a9a
jreyes8631 <jreyes8631@gmail.com>
2020-09-11 21:54:48 -0400

    rendering the show page from login and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,28 +10,28 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect "user/#{@user.id}"
     else
-
+      erb :signup
     end
 
   end
 
   get '/signup' do
-    erb :'users/signup'
+    erb :signup
   end
 
   post 'signup' do
     @user = User.new(email: params[:email], password: params[:password])
     if @user.save
       session[:id] = @user.id
-      redirect "/"
+      redirect "user/#{@user.id}"
     else
-      erb :'users/signup'
+      erb :'signup'
     end
   end
   
-  get '/users/show' do 
-      erb :'users/show'
+  get '/users/:id' do
+    @user = User.find_by(id: params[:id])
+    erb :'/users/show'
   end
-
 
 end
