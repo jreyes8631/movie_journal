8a21c71a42f3e2f0da938cd5c3483fe149ba5910
jreyes8631 <jreyes8631@gmail.com>
2020-09-12 06:45:39 -0400

    add session controlers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+    get '/login' do
+     erb :'/sessions/login'
+    end
+
+    post '/login' do
+
+    user = User.find_by_email(params[:email])
+        if user && user.authenticate(params[:password])
+            session[:id] = user.id
+            redirect "/"
+        else
+            @error = "Incorrect email or password"
+            erb :'/sessions/login'
+        end
+    end
+
+
+    delete '/logout' do
+        session.clear
+        redirect "/"
+    end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,5 @@
 class UsersController < ApplicationController
 
-  get "/login" do
-    erb :login
-  end
-
-  post '/login' do
-    @user = User.find_by(email: params[:email])
-    if @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect "user/#{@user.id}"
-    else
-      redirect 'signup'
-    end
-
-  end
 
   get '/signup' do
     erb :signup
@@ -24,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(email: params[:email], password: params[:password])
     if @user.save
       session[:id] = @user.id
-      redirect "user/#{@user.id}"
+      redirect "/"
     else
       erb :signup
     end
