1e1e284c0c90853e7a945d7f2d7ea1b90de37340
jreyes8631 <jreyes8631@gmail.com>
2020-09-12 16:49:17 -0400

    add logout functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-   configure do
+  configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     set :sessions, true
@@ -14,26 +14,26 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
-  helpers do
+ private
 
-    def current_user
-      @current_user = User.find_by(id: session[:user_id])
-    end
-   
-    def logged_in?
-      !!current_user
-    end
-
-    def redirect_if_not_logged_in
-      if !logged_in?
-        flash[:error] = "You must be logged in to view that page"
-        redirect request.referrer || "/login"
-      end
-    end
-
-   
-
-
+  def current_user
+   @current_user = User.find_by(id: session[:user_id])
   end
+   
+  def logged_in?
+    !!current_user
+  end
+
+  def redirect_if_not_logged_in
+    if !logged_in?
+      flash[:error] = "You must be logged in to view that page"
+      redirect request.referrer || "/login"
+     end
+  end
+
+   
+
+
+  
 
 end
