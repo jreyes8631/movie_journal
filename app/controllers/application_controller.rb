76d6beb38a9053bcf0407428347759f3bb3d6091
jreyes8631 <jreyes8631@gmail.com>
2020-09-12 06:54:57 -0400

    add redirect_if_not_logged_in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,14 +15,23 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do
+
+    def current_user
+      @current_user = User.find_by(id: session[:user_id])
+    end
    
     def logged_in?
       !!current_user
     end
 
-    def current_user
-      @current_user = User.find_by(id: session[:user_id])
+    def redirect_if_not_logged_in
+      if !logged_in?
+        flash[:error] = "You must be logged in to view that page"
+        redirect request.referrer || "/login"
+      end
     end
+
+   
 
 
   end
