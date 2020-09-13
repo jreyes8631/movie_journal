d27e4ad3d044b889889c22c285716a45397f6bde
jreyes8631 <jreyes8631@gmail.com>
2020-09-13 11:08:48 -0400

    add movie show render

--- app/controllers/movies_controler.rb
+++ app/controllers/movies_controler.rb
@@ -1,15 +1,29 @@
 class MoviesController < ApplicationController
 
-    get "/movies" do
-        @movies = Movie.all
-       erb :"/movies/index.html"
+  get "/movies" do
+      @movies = Movie.all
+      erb :"/movies/index.html"
+  end
+  
+  get "/movies/new" do
+    redirect_if_not_logged_in
+    @movie = Movie.new
+      erb :"/movies/new.html"
+  end
+  
+  post "/movies" do
+    redirect_if_not_logged_in
+    @movie = current_user.movies.build(title: params[:post][:title], content: params[:post][:content])
+    if @post.save
+      redirect "/movies"
+    else
+      erb :"/movies/new.html"
     end
-    
-    get "/movies/new" do
-      redirect_if_not_logged_in
-      @movie = Movie.new
-        erb :"/movies/new.html"
-    end
+  end
 
+  get "/movies/:id" do
+    @movie = Movie.find(params[:id])
+    erb :"/movies/show.html"
+  end
     
 end
