c37dbcbee5701126479c580f2c21e521d3e46b68
jreyes8631 <jreyes8631@gmail.com>
2020-09-13 08:49:59 -0400

    add route to index.html and new.html

--- app/controllers/movies_controler.rb
+++ app/controllers/movies_controler.rb
@@ -1,3 +1,12 @@
 class MoviesController < ApplicationController
-  
+
+    get "/movies" do
+        @movies = Movie.all
+       erb :"/movies/index.html"
+    end
+    
+    get "/movies/new" do
+      
+        erb :"/movies/new.html"
+    end
 end
